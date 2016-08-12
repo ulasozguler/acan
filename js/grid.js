7a658af4e64c6e57b9812528762a3246b7c24837
ulas <ulasozguler@gmail.com>
2016-08-13 02:04:19 +0300

    added step + behaviour + demo.

--- js/grid.js
+++ js/grid.js
@@ -7,7 +7,7 @@ class Grid {
 
 	draw(domElementID) {
 		var canvas = document.getElementById(domElementID);
-		var cellSize = 25;
+		var cellSize = 10;
 		canvas.setAttribute("width", (this.width * cellSize) + 2 + '');
 		canvas.setAttribute("height", (this.height * cellSize) + 2 + '');
 
@@ -15,24 +15,24 @@ class Grid {
 		ctx.fillStyle = "#000000";
 		ctx.imageSmoothingEnabled = false;
 
-		for (x = 0; x <= this.width; x++) {
+		for(x = 0; x <= this.width; x++) {
 			ctx.moveTo(1 + x * cellSize, 1);
 			ctx.lineTo(1 + x * cellSize, 1 + this.height * cellSize);
 			ctx.stroke();
 		}
 
-		for (y = 0; y <= this.height; y++) {
+		for(y = 0; y <= this.height; y++) {
 			ctx.moveTo(1, 1 + y * cellSize);
 			ctx.lineTo(1 + this.height * cellSize, 1 + y * cellSize);
 			ctx.stroke();
 		}
 
-		for (var x in this.cells) {
+		for(var x in this.cells) {
 			if(!this.cells.hasOwnProperty(x)) continue;
-			if (this.cells[x]) {
-				for (var y in this.cells[x]) {
+			if(this.cells[x]) {
+				for(var y in this.cells[x]) {
 					if(!this.cells[x].hasOwnProperty(y)) continue;
-					if (this.cells[x][y] == 1) {
+					if(this.cells[x][y] == 1) {
 						ctx.fillRect(x * cellSize, y * cellSize, cellSize, cellSize);
 					}
 				}
@@ -41,7 +41,7 @@ class Grid {
 	}
 
 	fill(x, y) {
-		if (!this.cells[x]) {
+		if(!this.cells[x]) {
 			this.cells[x] = {};
 		}
 		this.cells[x][y] = 1;
